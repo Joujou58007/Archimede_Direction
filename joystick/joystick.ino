1dcfdada3e35d9596f74084dabc3d3de1863323d
Joujou <leducjulien169@gmail.com>
2025-01-17 09:02:52 -0500

    added  three more servo

--- joystick/joystick.ino
+++ joystick/joystick.ino
@@ -1,10 +1,5 @@
 #include <Servo.h> // Inclure la bibliothèque Servo
 
-
-// Définition des broches du joystick (axes X et Y)
-const int joystickXPin = A0;  // Axe X connecté à la broche analogique A0
-const int joystickYPin = A1;  // Axe Y connecté à la broche analogique A1
-
 // Définition des catégories sous forme d'entiers (ou utilisez des énumérations si vous préférez)
 #define FULL_GAUCHE 5
 #define HALF_GAUCHE 4
@@ -13,14 +8,31 @@ const int joystickYPin = A1;  // Axe Y connecté à la broche analogique A1
 #define FULL_DROITE 1
 #define OUT_OF_RANGE 0
 
-Servo myServo; // Créer un objet Servo
+
+// Définition des broches du joystick (axes X et Y)
+const int joystickXPin = A0;  // Axe X connecté à la broche analogique A0
+const int joystickYPin = A1;  // Axe Y connecté à la broche analogique A1
+
+const int UPPER_LEFT_SERVO_PORT = 11;
+const int UPPER_RIGHT_SERVO_PORT = 10;
+const int DOWN_RIGHT_SERVO_PORT = 9;
+const int DOWN_LEFT_SERVO_PORT = 6;
+
+// Servo myServo; // Créer un objet Servo
+Servo UpperLeftServo;
+Servo UpperRightServo;
+Servo DownRightServo;
+Servo DownLeftServo;
 
 void setup() {
   // Démarrer la communication série
   Serial.begin(9600);
 
-  // Connecter le servomoteur à la broche 9 (ou une autre broche PWM)
-  myServo.attach(9);
+  // Connecter les servomoteurs aux branches 11, 10, 9 et 6
+  UpperLeftServo.attach(UPPER_LEFT_SERVO_PORT);
+  UpperRightServo.attach(UPPER_RIGHT_SERVO_PORT);
+  DownRightServo.attach(DOWN_RIGHT_SERVO_PORT);
+  DownLeftServo.attach(DOWN_LEFT_SERVO_PORT);
 }
 
 void loop() {
@@ -42,8 +54,12 @@ void loop() {
   // Convertir la position X du joystick en un angle pour le servomoteur
   int servoAngle = map(joystickXValue, 0, 1023, 0, 180);  // Mapper la valeur de 0 à 1023 à un angle de 0 à 180
 
-  // Positionner le servomoteur à l'angle calculé
-  myServo.write(servoAngle);
+  // Positionner les servomoteurs à l'angle calculé
+  UpperLeftServo.write(servoAngle);
+  UpperRightServo.write(servoAngle);
+  DownRightServo.write(servoAngle);
+  DownLeftServo.write(servoAngle);
+
 
   // Ajouter un petit délai pour la lisibilité et éviter des mouvements trop rapides
   delay(100);  // Délai de 100 ms pour rendre les mouvements plus fluides
